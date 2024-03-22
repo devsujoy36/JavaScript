8674062d0dabf8465d68c72519a53cbdee244568
devsujoy36 <sujoydaspc2023@gmail.com>
2024-03-23 03:51:02 +0600

    maxmath

---
+++ Math/tallest.js
@@ -0,0 +1,26 @@
+function Largest(numbers){
+    let largest = numbers[0];
+    for(let i = 0; i < numbers.length; i++){
+        const index = i;
+        const element = numbers[index];
+        if(element>largest){
+            largest = element;
+        }
+    }
+    return largest;
+}
+function Lowest(numbers){
+    let lowest = numbers[0];
+    for(let i = 0; i < numbers.length; i++){
+        const index = i;
+        const element = numbers[index];
+        if(lowest>element){
+            lowest = element ;
+        }
+    }
+    return lowest;
+}
+
+const heights = [167, 190, 120, 165, 137, 545];
+const tallest = Lowest(heights);
+console.log('Lowest persons length is: ',tallest);
