de2a0b25dfc193dc1d4b9d679b3745f28bf1a2a2
devsujoy36 <sujoydaspc2023@gmail.com>
2024-03-23 03:58:02 +0600

    maxmath

---
+++ Math/reverse.js
@@ -0,0 +1,10 @@
+function rerverseString(text){
+    for(let i = 0; i < text.length; i++){
+        const element = text[i];
+        console.log(element);
+    }
+
+}
+
+const myString = 'i am a good boy';
+rerverseString(myString);
