b8d51d8a954ffd631f8e16ea6192121b0acf7410
devsujoy36 <sujoydaspc2023@gmail.com>
2024-03-22 17:47:59 +0600

    case.js

--- ZJsProblems/case.js
+++ ZJsProblems/case.js
@@ -1,2 +1,8 @@
-const userName - 'blackPink'
-const userInputame - 'blackPink'
+const userName = 'blackPink';
+const userInput = 'blackPinK';
+if(userName.toLowerCase() === userInput.toLowerCase()){
+    console.log('valid data');
+}
+else{
+    console.log('invalid')
+}
