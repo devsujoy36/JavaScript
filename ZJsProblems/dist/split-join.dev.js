34f42ffc2a8373433853c8e0b35c8870aed3a805
devsujoy36 <sujoydaspc2023@gmail.com>
2024-03-22 18:52:22 +0600

    split-join.js

---
+++ ZJsProblems/dist/split-join.dev.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var song = 'ami tomar didhay bachi. Tomar didhay pure chai. emon didha prithibite dhiskew diskew koira khai.';
+console.log(song);
+var part = song.split(' ');
+var sentence = song.split('.');
+
+var _char = song.split('');
+
+console.log(song.length());
+console.log(part);
+console.log(sentence);
+console.log(_char);
--- ZJsProblems/split-join.js
+++ ZJsProblems/split-join.js
@@ -1,9 +1,9 @@
- const song = 'ami tomar didhay bachi. Tomar didhay pure chai. emon didha prithibite dhiskew diskew koira khai.';
- console.log(song);
+const song = 'ami tomar didhay bachi. Tomar didhay pure chai. emon didha prithibite dhiskew diskew koira khai.';
+console.log(song);
  
- const part = song.split(' '); //er sahajje uporer string data gulo ke proti word e alada kore dibe ;
- const sentence = song.split('.'); //er sahajje uporer string data gulo ke sentece onujai vag kore dibe;
- const char = song.split(''); //er sahajje uporer string e thaka prottekti datar alphabate gulo ke alada kore dibe;
+const part = song.split(' ');
+const sentence = song.split('.');
+const char = song.split('');
 
 console.log(part);
 console.log(sentence);
