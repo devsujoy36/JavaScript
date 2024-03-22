6809153868c5d8603e13a4a084c2ac64de9562da
devsujoy36 <sujoydaspc2023@gmail.com>
2024-03-23 04:17:22 +0600

    maxmath

--- Math/reverse.js
+++ Math/reverse.js
@@ -1,8 +1,18 @@
-function rerverseString(text){
+function forwordString(text){
     for(let i = 0; i < text.length; i++){
         const element = text[i];
         console.log(element);
     }
+
+}
+function rerverseString(text){
+    let reverse = '';
+    for(let i = text.length-1 ; i >= 0; i--){
+        const element = text[i];
+        reverse = reverse + element;
+        console.log(element,reverse);
+    }
+    return reverse;
 
 }
 
