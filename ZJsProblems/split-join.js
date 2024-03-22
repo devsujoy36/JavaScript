bd2ae77d15e13e985471766099da3d53caf3f33d
devsujoy36 <sujoydaspc2023@gmail.com>
2024-03-22 19:06:30 +0600

    split-join.js

--- ZJsProblems/split-join.js
+++ ZJsProblems/split-join.js
@@ -1,10 +1,17 @@
-const song = 'ami tomar didhay bachi. Tomar didhay pure chai. emon didha prithibite dhiskew diskew koira khai.';
-console.log(song);
+const song = ' ami tomar didhay bachi. Tomar didhay pure chai. emon didha prithibite dhiskew diskew koira khai.';
+console.log('Full Sentence of song is: ', song);
  
-const part = song.split(' ');
+const perword = song.split(' ');
 const sentence = song.split('.');
 const char = song.split('');
+console.log('split of song Per word is: ', perword);
+console.log('split of song sentence is: ', sentence);
+console.log('split of song alphabate is: ', char);
 
-console.log(part);
-console.log(sentence);
-console.log(char);
+
+const partial = song.slice(4,10);
+console.log('slice value is: ',partial);
+
+const partial2 = song.substring(4,10);
+console.log('substring value is: ', partial2);
+
