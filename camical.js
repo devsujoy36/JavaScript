cb12eb8d704c1396f49da1fa22ad1143bd468d7f
devsujoy36 <sujoydaspc2023@gmail.com>
2024-03-23 02:49:24 +0600

    math.js

--- camical.js
+++ camical.js
@@ -3,12 +3,11 @@ let second = 7;
 console.log(first,second);
 
 //approach 1
-// const temp = first;
-// first = second;
-// second = temp;
-// console.log(first,second);
+const temp = first;
+first = second;
+second = temp;
+console.log(first,second);
 
 // approch 2 destructuring
-
 [first, second] = [second, first] ;
 console.log(first,second);
