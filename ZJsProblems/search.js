6aa2e31a1749b89427358a1a96a7ee649293bf19
devsujoy36 <sujoydaspc2023@gmail.com>
2024-03-22 18:14:53 +0600

    search.js

--- ZJsProblems/search.js
+++ ZJsProblems/search.js
@@ -1,5 +1,6 @@
 const song = 'Ami tomar didhay bachi, tomar didhay more jai';
 const lowerCase = song.toLowerCase();
-
 const doesExist = lowerCase.includes('ami');
+const textIndex = lowerCase.indexOf('tomar');
 console.log(doesExist);
+console.log(textIndex);
