6891d7e79fad7790341a61002b1034fec47caf0a
devsujoy36 <sujoydaspc2023@gmail.com>
2024-03-22 19:11:53 +0600

    split-join.js

--- ZJsProblems/split-join.js
+++ ZJsProblems/split-join.js
@@ -1,6 +1,6 @@
-const song = ' ami tomar didhay bachi. Tomar didhay pure chai. emon didha prithibite dhiskew diskew koira khai.';
+const song = ' ami tomar didhay bachi. Tomar didhay pure chai. emon didha prithibite dhiskew diskew koira khai. ';
 console.log('Full Sentence of song is: ', song);
- 
+
 const perword = song.split(' ');
 const sentence = song.split('.');
 const char = song.split('');
@@ -9,9 +9,19 @@ console.log('split of song sentence is: ', sentence);
 console.log('split of song alphabate is: ', char);
 
 
-const partial = song.slice(4,10);
-console.log('slice value is: ',partial);
+const partial = song.slice(5,10);
+console.log('slice value is:',partial);
 
-const partial2 = song.substring(4,10);
-console.log('substring value is: ', partial2);
+const partial2 = song.substring(5,10);
+console.log('substring value is:', partial2);
+
+const trim = song.trim();
+console.log(trim);
+
+const trimStart = song.trimStart();
+console.log(trimStart);
+
+const trimEnd = song.trimEnd();
+console.log(trimEnd);
+
 
