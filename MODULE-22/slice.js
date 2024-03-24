b9569dfe56c4f7d271fb904ddcb335c07476285c
devsujoy36 <sujoydaspc2023@gmail.com>
2024-03-25 02:27:01 +0600

    splice.js

--- MODULE-22/slice.js
+++ MODULE-22/slice.js
@@ -1,5 +1,6 @@
-//VarName.slice(startingindex, endindex);
-const friend = [12,35,15,15,38,16];
+//VarName.slice(startingindex, endindex)
+const friend = [12, 35, 15, 15, 38, 16, 33, 66, 64, 18, 48, 55, 26];
+console.log('Original array before slice:', friend);
 const partial = friend.slice(2, 5);
-console.log(partial);
-
+console.log('Splie Part:', partial);
+console.log('Current Array after slice:', friend);
--- MODULE-22/splice.js
+++ MODULE-22/splice.js
@@ -5,6 +5,11 @@
 // will change the original array
 //er sahajje array er majhkhan ba jekono index number theke item badd deya jay 
 
-const friend = [12, 35, 15, 15, 38, 16];
-const partial = friend.splice(2, 5);
-console.log(partial);
+const friend = [12,35,15,15,38,16,33,66,64,18,48,55,26];
+console.log('Original array before splie:',friend);
+const partial = friend.splice(2, 5); 
+console.log('Splie Part:',partial);
+console.log('Current Array after splie:',friend);
+
+// splice er khettre 
+// VarName.splice(startingindex, koiytadeletekortechaitarsongkha);
