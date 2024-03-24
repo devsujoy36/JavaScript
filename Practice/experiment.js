264c9668ce01b9cfb7e218437bb36c0a23152b1d
devsujoy36 <sujoydaspc2023@gmail.com>
2024-03-25 01:39:28 +0600

    UDATE

--- Practice/experiment.js
+++ Practice/experiment.js
@@ -1,14 +1,14 @@
-// function kuchtogarbar(number){
-//     let randomvalue = [];
-//     for(let i = 0; i < number.length; i++){
-//         let random = Math.round(Math.random() * 500);
-//         console.log(random);
-//         randomvalue.push(random);
-//     }
-//     console.log(randomvalue);
-//     return randomvalue;
-// }
-// kuchtogarbar(number);
+function kuchtogarbar(number){
+    let randomvalue = [];
+    for(let i = 0; i < number.length; i++){
+        let random = Math.round(Math.random() * 500);
+        console.log(random);
+        randomvalue.push(random);
+    }
+    console.log(randomvalue);
+    return randomvalue;
+}
+kuchtogarbar(number);
 let a = [];
 const random = Math.round(Math.random() * 500);
 a.push(random);
