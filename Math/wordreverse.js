d50d77a48a912cecbf4630e7fe459dc24a92157f
devsujoy36 <sujoydaspc2023@gmail.com>
2024-03-25 00:18:39 +0600

    maxmath

--- Math/wordreverse.js
+++ Math/wordreverse.js
@@ -1,7 +1,7 @@
-function reverseWord(str){
+function reverseWord(str) {
     const word = str.split(' ');
-    const restlt = [];
-    for(let i = word.length - 1; i >= 0; i--){
+    const result = [];
+    for (let i = word.length - 1; i >= 0; i--) {
         const element = word[i];
         result.push(element);
     }
@@ -11,3 +11,4 @@ function reverseWord(str){
 
 const str = 'hi plz help me';
 const varu = reverseWord(str);
+console.log(varu);
