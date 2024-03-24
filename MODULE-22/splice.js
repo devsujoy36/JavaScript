81898ba0fdb04318e38dcbd34b7d77920613a323
devsujoy36 <sujoydaspc2023@gmail.com>
2024-03-25 02:47:59 +0600

    rem-duplicate.js

--- MODULE-22/splice.js
+++ MODULE-22/splice.js
@@ -1,9 +1,9 @@
-//splice er sahajje array theke ekta ongso kete 
-// remove elements from an array 
-// and if necessary, inserts new elements in their place
-// returning the deleted elements
-// will change the original array
-//er sahajje array er majhkhan ba jekono index number theke item badd deya jay 
+// splice er khettre
+// VarName.splice(startindex, dlt-itm-num, add-items,add-iteams);
+
+// kono ekti array theke ekti ongso kete niya asa splice er kaj
+// tobe splice dara original array change hoy
+// er sahajje notun iteam array te insert kora jay
 
 const friend = [12,35,15,15,38,16,33,66,64,18,48,55,26];
 console.log('Original array before splie:',friend);
@@ -11,5 +11,3 @@ const partial = friend.splice(2, 5 ,66,52,14);
 console.log('Splie Part:',partial);
 console.log('Current Array after splie:',friend);
 
-// splice er khettre 
-// VarName.splice(startindex, dlt-itm-num, add-items,add-iteams);
