b9786516754e78bfd27b43e01a73bb7ca1feadf7
devsujoy36 <sujoydaspc2023@gmail.com>
2024-03-26 04:16:17 +0600

    summary.html

--- MODULE-24/firstfile.js
+++ MODULE-24/firstfile.js
@@ -1,5 +1,12 @@
 console.log('First file');
 
 const allLi = document.getElementsByTagName('li');
+console.log(allLi);
 
 const allTitle = document.getElementsByClassName('section-title');
+console.log(allTitle);
+
+const secondSection = document.getElementById('Second-section');
+console.log(secondSection);
+secondSection.style.color = 'blue';
+secondSection.style.fontSize = '29px'
