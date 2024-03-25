3267763313823d5d7219fdac9888955687d8a566
devsujoy36 <sujoydaspc2023@gmail.com>
2024-03-26 03:18:49 +0600

    append.js

--- MODULE-24/dom.js
+++ MODULE-24/dom.js
@@ -1,15 +1,39 @@
-const sections = document.querySelectorAll('section');
-for (const section of sections) {
-    section.style.border = '2px solid steelblue';
-    section.style.marginBottom = '5px';
-    section.style.borderRadius = '15px';
-    section.style.padding = '10px';
-    section.style.backgroundColor = 'lightgray';
-    section.style.color = 'black';
-    section.style.fontFamily = 'Segoe UI';
-
+const liCollection = document.getElementsByTagName('li');
+for (const li of liCollection) {
+    // console.log(li.innerText);
+}
+const allHeading = document.getElementsByTagName('h1');
+for (const h1 of allHeading) {
+    // console.log(h1.innerText);
 }
 
-const placesContainer = document.getElementById('places-container');
-placesContainer.classList.add('text-center');
-placesContainer.classList.remove('large-text');
+const title = document.getElementById('fruits-title');
+title.innerText = 'Fruits changed by JS';
+
+const places = document.getElementsByClassName('importent-places');
+for (const place of places) {
+    // console.log(place.innerText);
+}
+
+
+const someLi = document.querySelectorAll('.fruits-container li');
+for (const li of someLi) {
+    // console.log(li.innerText);
+
+}
+const titile = document.getElementById('hang');
+const getAttribute = titile.getAttribute('class');
+console.log('GetAtribute holo:', getAttribute);
+
+const ClassList = titile.classList;
+console.log('Frist ClassList', ClassList);
+console.log(titile.classList.add('purple.bg'));
+console.log('Current ClassList', ClassList);
+
+
+// const setAttribute = document.getElementById('fruits-title');
+// const a = setAttribute.setAttribute(setAttribute, 'Tooltip set by JavaScript');
+// console.log('setattribute',a);
+
+const b = document.getElementsByClassName('fruits-container')[0].innerHTML;
+console.log('InnerHTML', b);
