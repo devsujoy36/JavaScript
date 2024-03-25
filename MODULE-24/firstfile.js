4e9b035238713bf8ebe2416b543771427990ad76
devsujoy36 <sujoydaspc2023@gmail.com>
2024-03-26 04:18:49 +0600

    summary.html

--- MODULE-24/firstfile.js
+++ MODULE-24/firstfile.js
@@ -9,4 +9,8 @@ console.log(allTitle);
 const secondSection = document.getElementById('Second-section');
 console.log(secondSection);
 secondSection.style.color = 'blue';
-secondSection.style.fontSize = '29px'
+secondSection.style.fontSize = '29px';
+secondSection.style.backgroundColor = 'red';
+
+
+
