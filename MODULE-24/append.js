763d6170130a6f515adc3889589eb662e5dba5fb
devsujoy36 <sujoydaspc2023@gmail.com>
2024-03-26 03:57:51 +0600

    append.js

--- MODULE-24/append.js
+++ MODULE-24/append.js
@@ -34,14 +34,15 @@ ul.appendChild(li3);
 
 mainContainer.appendChild(section);
 
-const sectionDress = document.createElement(section);
+
+const sectionDress = document.createElement("section");
 sectionDress.innerHTML = `
-<h1>My Dress Section</h1>
-        <ul>
-            <li>Pant</li>
-            <li>Panjabi</li>
-            <li>Fotua</li>
-            <li>jangu</li>
-        </ul>
-`
+            <h1>My Dress Section</h1>
+            <ul>
+                <li>Pant</li>
+                <li>Panjabi</li>
+                <li>Fotua</li>
+                <li>Jangu</li>
+            </ul>
+        `;
 mainContainer.appendChild(sectionDress);
