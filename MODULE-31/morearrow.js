22ecd4efc7120127cfadba0681839dc0e0e2953d
devsujoy36 <sujoydaspc2023@gmail.com>
2024-04-20 15:36:28 +0600

    add

---
+++ MODULE-31/morearrow.js
@@ -0,0 +1,8 @@
+const defference = (a , b) => a - b;
+const multiply = (first, second, third) => first * second * third;
+
+const getAge = person => person.age;
+const student = {name: 'sujoy', age: 20}
+const age = getAge(student)
+console.log(age);
+
