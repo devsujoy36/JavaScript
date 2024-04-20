3199cde34da4aa34cb144e76836e83b3b87c596e
devsujoy36 <sujoydaspc2023@gmail.com>
2024-04-20 16:00:56 +0600

    add

--- MODULE-31/spread.js
+++ MODULE-31/spread.js
@@ -1,2 +1,9 @@
 const max = Math.max(55,21,31,65,69);
 console.log(max);
+
+
+
+const numbers = [44,56,15,34,87,57,66];
+const arrayMax = Math.max(numbers)
+console.log(numbers);
+console.log(arrayMax);
