7b56d8c22af7357bcfd8cc5fff602eaa9d8ac834
devsujoy36 <sujoydaspc2023@gmail.com>
2024-04-20 16:09:45 +0600

    add

--- MODULE-31/spread.js
+++ MODULE-31/spread.js
@@ -1,9 +1,19 @@
 const max = Math.max(55,21,31,65,69);
-console.log(max);
+// console.log(max);
 
 
+//spread ...
+const numbers = [44, 56, 15, 34, 87, 57, 66];
+const arrayMax = Math.max(...numbers)
+// console.log(...numbers);
+// console.log(arrayMax);
 
-const numbers = [44,56,15,34,87,57,66];
-const arrayMax = Math.max(numbers)
-console.log(numbers);
-console.log(arrayMax);
+//use speread operator to copy
+const friends = [44, 56, 15, 34, 87, 57, 66];
+const bondhu = friends;
+const dosto = [...friends]
+console.log(dosto);
+friends.push(222);
+console.log(dosto);
+
+
