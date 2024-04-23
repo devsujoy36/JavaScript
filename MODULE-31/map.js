cc3196ad4eab2c977cafe9827ec3d8891bac27bb
devsujoy36 <sujoydaspc2023@gmail.com>
2024-04-23 14:52:44 +0600

    add

--- MODULE-31/map.js
+++ MODULE-31/map.js
@@ -1,1 +1,16 @@
 const numbers = [4,5,6,7];
+
+// const doubled = [];
+// for (const num of numbers) {
+//     const double = num * 2;
+//     doubled.push(double);
+// }
+// console.log(doubled);
+
+const result = numbers.map();
+
+
+
+
+
+
