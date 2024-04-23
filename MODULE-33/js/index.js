7c37d6695e5123a812f7d6cae5522b19d23b0b9a
devsujoy36 <sujoydaspc2023@gmail.com>
2024-04-24 03:00:00 +0600

    module33

--- MODULE-33/js/index.js
+++ MODULE-33/js/index.js
@@ -7,7 +7,6 @@ function loadUsers2() {
 function displayUsers2(data) {
     const ul = document.getElementById('users-list');
     for (const user of data) {
-        console.log(user.username);
         const li = document.createElement('li');
         li.innerText = user.name;
         ul.appendChild(li);
