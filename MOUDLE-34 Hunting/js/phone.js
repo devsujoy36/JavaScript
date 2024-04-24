bf65b812134a63d3667407844039cb47ad1f7de0
devsujoy36 <sujoydaspc2023@gmail.com>
2024-04-24 22:07:35 +0600

    module33

--- MOUDLE-34 Hunting/js/phone.js
+++ MOUDLE-34 Hunting/js/phone.js
@@ -69,16 +69,16 @@ const toggleLoadingSpinner = (isLoading) => {
     const loadingSpinner = document.getElementById('loading-spinner');
     if (isLoading) {
         loadingSpinner.classList.remove('hidden')
+
     }
     else{
         loadingSpinner.classList.add('hidden')
     }
-
-
-
 }
 
-
+const handleShowAll = () => {
+    handleSearch()
+}
 
 
 
