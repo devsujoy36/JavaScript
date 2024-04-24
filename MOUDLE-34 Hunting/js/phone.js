bdaa2f68fa3f478a1518f52935e609e99d792023
devsujoy36 <sujoydaspc2023@gmail.com>
2024-04-25 04:12:53 +0600

    add new things

--- MOUDLE-34 Hunting/js/phone.js
+++ MOUDLE-34 Hunting/js/phone.js
@@ -1,52 +1,68 @@
-const loadPhone = async (searchText,isShowAll) => {
-    const url = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
-    const data = await url.json();
+const loadPhone = async (searchText, isShowAll) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
+    const data = await res.json();
     const phones = data.data;
     displayPhones(phones,isShowAll);
 }
 
 const displayPhones = (phones, isShowAll) => {
+    console.log(phones);
     const phoneContainer = document.getElementById('phone-container');
     phoneContainer.textContent = '';
-    //display 10 phone only
     const showAllContainer = document.getElementById('show-all-container');
-    if (phones.length > 10 && !isShowAll) {
-        phones = phones.slice(0, 10);
+    if (phones.length > 12 && !isShowAll) {
         showAllContainer.classList.remove('hidden');
     }
     else {
         showAllContainer.classList.add('hidden');
     }
     
+    console.log('is Show All ', isShowAll);
 
+    if (!isShowAll) {
+        phones = phones.slice(0, 12);
+    }
 
-
-
-
-
-
-
-
-    //display all phones
     phones.forEach(phone => {
         // console.log(phone);
-        const phoneDiv = document.createElement('div');
-        phoneDiv.classList = `card bg-gray-100 shadow-xl`;
-        phoneDiv.innerHTML = `
-        <div class="card p-4">
-            <img class="card-img-top" src=${phone.image} alt="..." />
+        const phoneCard = document.createElement('div');
+        phoneCard.classList = `card bg-gray-100 shadow-xl`;
+        phoneCard.innerHTML = `
+            <figure ><img class="m-4" src=${phone.image} alt="Shoes" /></figure>
             <div class="card-body">
                 <h2 class="card-title">${phone.phone_name}</h2>
                 <p>${phone.slug}</p>
-                <button onclick="handleShowDetails('${phone.slug}')" class="btn btn-primary">Show Details</button>                   
+                <div class="card-actions justify-center">
+                    <button onclick="handleShowDetails('${phone.slug}')" class="btn btn-primary">Show Details</button>
+                </div>
             </div>
-        </div>
-        `;
-        phoneContainer.appendChild(phoneDiv);
-    })
+        `
+        phoneContainer.appendChild(phoneCard);
+    });
     //hide loading spinner
     toggleLoadingSpinner(false);
 }
+
+
+//handle show details
+const handleShowDetails = async (id) => {
+    console.log('click show details', id);
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${id}`)
+    const data = await res.json();
+    const phone = data.data;
+
+    showPhoneDetails(phone);
+
+}
+
+const showPhoneDetails = (phone) => {
+    console.log(phone);
+    const phoneName = document.getElementById('phone-name');
+
+
+    show_details_modal.showModal();
+}
+
 
 //handle search button
 const handleSearch = (isShowAll) => {
@@ -55,10 +71,6 @@ const handleSearch = (isShowAll) => {
     const searchText = searchField.value;
     // console.log(searchText);
     loadPhone(searchText, isShowAll);
-}
-
-const handleShowAll = () => {
-    handleSearch(true);
 }
 
 const toggleLoadingSpinner = (isLoading) => {
@@ -71,32 +83,9 @@ const toggleLoadingSpinner = (isLoading) => {
     }
 }
 
-//handle show details
-const handleShowDetails = async (id) => {
-    // console.log('click show details', id);
-    const url = await fetch(`https://openapi.programming-hero.com/api/phones?search=${id}`)
-    const data = await url.json();
-    const phone = data.data;
-
-    displayPhoneDetails(phone);
-
+const handleShowAll = () => {
+    handleSearch(true);
 }
-
-const displayPhoneDetails = (phone) => {
-    console.log(phone);
-    const phoneName = document.getElementById('show-details-phone-name');
-    phoneName.innerText = phone.name;
-
-    
-
-    show_details_modal.showModal();
-}
-
-
-
-
-
-
 
 
 
